0c16b6149cd5d3794711a709566970c433488a18
MohammedSaadyAhmed <121351707+MohammedSaadyAhmed@users.noreply.github.com>
2023-07-11 07:49:36 +0300

    Update resources.tf

--- Terraform-2/resources.tf
+++ Terraform-2/resources.tf
@@ -1,1 +1,32 @@
-
+resource "aws_vpc" "vpc1" {
+  cidr_block = var.CIDRS["vpc-cidr"]
+  tags = {
+    "Name" = "terraform1-vpc"
+    project = "sprints"
+  }
+}
+resource "aws_subnet" "Public_Subnet" {
+    vpc_id     = aws_vpc.vpc1.id
+    cidr_block= var.CIDRS["public-subnet-cidr"]
+    tags = {
+        Name = "terraform1-public-subnet"
+  }
+} 
+resource "aws_subnet" "Private_Subnet" {
+    vpc_id     = aws_vpc.vpc1.id
+    cidr_block= var.CIDRS["private-subnet-cidr"]
+    tags = {
+        Name = "terraform1-private-subnet"
+  }
+} 
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.vpc1.id
+  
+}
+resource "aws_route_table" "terraform-public-rt" {
+  vpc_id = aws_vpc.vpc1.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
