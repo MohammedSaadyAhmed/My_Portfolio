dff9db38bb6f6a3900b8841568ed6605ef1a4d66
MohammedSaadyAhmed <121351707+MohammedSaadyAhmed@users.noreply.github.com>
2023-07-11 12:15:49 +0300

    Update outputs.tf

--- Terraform-2/outputs.tf
+++ Terraform-2/outputs.tf
@@ -1,3 +1,9 @@
+# print the ec2's public ipv4 address
+
+output "public_ipv4_address" {
+  value = aws_instance.terraform1_instance.id
+}
+
 output "public_ip" {
   value = aws_instance.terraform1_public_instance.public_ip
 }
