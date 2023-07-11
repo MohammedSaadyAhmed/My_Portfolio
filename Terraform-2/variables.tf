212edba50a7f9e29142b35744ac2f756b3bc9822
MohammedSaadyAhmed <121351707+MohammedSaadyAhmed@users.noreply.github.com>
2023-07-11 11:52:14 +0300

    Update variables.tf

--- Terraform-2/variables.tf
+++ Terraform-2/variables.tf
@@ -1,1 +1,10 @@
-
+variable "CIDRS" {
+  description = " CIDR blocks "
+  type        = map 
+  default     = {
+    "vpc-cidr"            = "10.0.0.0/16"
+    "public-subnet-cidr"  = "10.0.0.0/24"
+    "private-subnet-cidr" = "10.0.1.0/24"
+    "all traffic"         = "0.0.0.0/0"
+  }
+}
